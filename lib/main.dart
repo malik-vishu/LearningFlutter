1b120245a5a674324581df79164d2f3a0f469d3b
malik-vishu <malik03ai@gmail.com>
2023-07-11 12:24:38 +0530

    Themes managed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/screens/first_screen.dart';
 import 'package:first_app/screens/login_screen.dart';
 import 'package:first_app/utils/all_routes.dart';
+import 'package:first_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +14,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: AllThemes.lightTheme(context),
+      darkTheme: AllThemes.darkTheme(context),
       initialRoute: AllRoutes.homeRoute,
       routes: {
         "/": (context) => const LoginPage(),
